1f52db0299f54f90d1065f2b9b64bb9eeed09c25
Dev. Pablo H. <devpabloh@gmail.com>
2024-08-15 17:07:48 -0300

    Update App.tsx

--- projeto_to_do_list_react/src/App.tsx
+++ projeto_to_do_list_react/src/App.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>To-Do List</h1>
+    <div>
+      <div>
+        <h1>To-Do List</h1>
+      </div>
     </div>
   );
 }
